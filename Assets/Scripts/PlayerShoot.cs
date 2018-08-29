4ef3b88f8b783d5eac133bbb298861853efeb9ff
unknown <e.g.aragon2015@gmail.com>
2018-08-28 21:53:05 -0500

    Player Shooting

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+
+
+
+
+
+    public GameObject bulletPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameObject newBullet = GameObject.Instantiate(bulletPrefab, this.transform.position, new Quaternion());
+            newBullet.GetComponent<BulletFly>().direction = new Vector2(0, 1);
+        }
+
+    }
+}
